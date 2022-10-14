9d992f2ee6310cfb5d138e7b7734409b4e3193b9
Eric <eng0130@gmail.com>
2022-10-13 22:28:30 -0400

    added explanation strings

--- static_var_demo.c
+++ static_var_demo.c
@@ -15,11 +15,10 @@ int localized(int a){
 }
 
 int main(){
-	printf("%d\n", stasis(1));
-	printf("%d\n", stasis(1));
+	printf("'b' initialized as static variable equal to 3.\n'a' equals 1.\nOutput of b+=a is %d\n", stasis(1));
+	printf("'b' retains its value of 4 for next function call.\n'a' equals to 1.\nOutput of b+=a is %d\n", stasis(1));
 
-	printf("%d\n", localized(1));
-	printf("%d\n", localized(1));
-
+	printf("'b' initialized as local variable equal to 3.\n'a' passed as argument and equals 1.\nOutput of b+=a is %d\n", localized(1));
+	printf("'b' re-initialized as local variable equal to 3.\n'a' equals 1.\nOutput of b+=a is %d\n", localized(1));
 	return(0);
 }
