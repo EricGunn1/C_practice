aab3b761f1b240eced25bdcea3f6f6028e3b2570
Eric <eng0130@gmail.com>
2022-10-13 23:35:11 -0400

    added hola and adios

--- heap_example.c
+++ heap_example.c
@@ -5,15 +5,25 @@
 
 int main(){
 	char *hello;
+	char *hola;
 	char goodbye[200];
+	char adios[200];
 	int mem_size = 200;
 
-	strcpy(goodbye, "This memory is on the stack");
+	strcpy(goodbye, "'goodbye' is on the stack");
 	printf("%s at address %p\n", goodbye, &goodbye);
+	strcpy(adios, "'adios' is also on the stack");
+	printf("%s at address %p\n", adios, &adios);
 	
 	hello = (char *) malloc(mem_size);
+	hola = (char *) malloc(mem_size);
 
-	strcpy(hello, "This memory is on the heap");
+	printf("\n");
+
+	strcpy(hello, "'hello' is on the heap");
 	printf("%s at address %p\n", hello, &hello);
+	strcpy(hola, "'hola' is also on the heap");
+	printf("%s at address %p\n", hola, &hola);
 	free(hello);
+	free(hola);
 }
