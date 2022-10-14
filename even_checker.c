1308d39684b8b204fa78a3b5fc3ed819bc3e0ea4
Eric <eng0130@gmail.com>
2022-10-13 21:04:31 -0400

    added command line user input

--- even_checker.c
+++ even_checker.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(){
-	int a = 8;
+int main(int nums, char *num[]){
+	int a;
+	char number[100];
+
+	strcpy(number, num[1]);
+	a = atoi(number);
 	if(a % 2 == 0){
 		printf("%d is even\n", a);
 	}
